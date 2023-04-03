66973d6be6165a5f958166d3cb44143f2028a909
Filipe Marques <62525275+filipe172020@users.noreply.github.com>
2023-04-02 22:31:58 -0300

    Add files via upload

---
+++ API_AtenSUS/API_AtenSUS/Models/Hospital.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace API_AtenSUS.Models
+{
+    public class Hospital
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = null;
+        [BsonElement("Nome")]
+        public string Nome { get; set; } = null;
+        [BsonElement("Numero")]
+        public int Numero { get; set; }
+        [BsonElement("Idade")]
+        public int Idade { get; set; }
+
+        [BsonElement("Altura")]
+        public int Altura { get; set; }
+
+        [BsonElement("Peso")]
+        public int Peso { get; set; }
+
+        [BsonElement("Endereco")]
+        public string Endereco { get; set; } = null;
+
+        [BsonElement("EspecialidadeEncaminhada")]
+        public string EspecialidadeEncaminhada { get; set; } = null;
+
+        [BsonElement("Temperatura")]
+        public int Temperatura { get; set; }
+
+        [BsonElement("Sintomas")]
+        public string Sintomas { get; set; } = null;
+    }
+
+}
---
+++ API_AtenSUS/API_AtenSUS/Services/HospitalServices.cs
@@ -0,0 +1,39 @@
+﻿using API_AtenSUS.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace API_AtenSUS.Services
+{
+    public class HospitalServices
+    {
+        private readonly IMongoCollection<Hospital> _HospitalCollection; //Nome aleatório
+
+        public HospitalServices(IOptions<HospitalDatabaseSettings> hospitalServices)
+        { //O nome dado é o mesmo da class, porém com h minúsculo
+
+            var mongoClient = new MongoClient(hospitalServices.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(hospitalServices.Value.DatabaseName);
+
+            _HospitalCollection = mongoDatabase.GetCollection<Hospital>
+                (hospitalServices.Value.HospitalCollectionName);
+        }
+
+        //Criação de métodos async da API, Get, Create, Update e Delete
+
+        public async Task<List<Hospital>> GetAsync() =>
+            await _HospitalCollection.Find(x => true).ToListAsync();
+
+        public async Task<Hospital> GetAsync(string id) =>
+            await _HospitalCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Hospital hospital) =>
+            await _HospitalCollection.InsertOneAsync(hospital);
+
+        public async Task UpdateAsync(string id, Hospital hospital) =>
+            await _HospitalCollection.ReplaceOneAsync(x => x.Id == id, hospital);
+
+        public async Task RemoveAsync(string id) =>
+            await _HospitalCollection.DeleteOneAsync(x => x.Id == id);
+
+    }
+}
