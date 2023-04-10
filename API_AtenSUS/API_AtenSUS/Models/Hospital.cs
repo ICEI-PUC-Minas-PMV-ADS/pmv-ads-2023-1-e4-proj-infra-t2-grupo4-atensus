217a800a5e7cd6828c177829c629d8d5228f4b9d
TiagoMiqueiasDesenvolvedor <tiagomiqueiasrodrigues@gmail.com>
2023-04-10 14:34:17 -0300

    teste-push

--- API_AtenSUS/API_AtenSUS/Models/Hospital.cs
+++ API_AtenSUS/API_AtenSUS/Models/Hospital.cs
@@ -9,17 +9,7 @@ namespace API_AtenSUS.Models
         public string Id { get; set; } = null;
         [BsonElement("Nome")]
         public string Nome { get; set; } = null;
-        [BsonElement("Numero")]
-        public int Numero { get; set; }
-        [BsonElement("Idade")]
-        public int Idade { get; set; }
-
-        [BsonElement("Altura")]
-        public int Altura { get; set; }
-
-        [BsonElement("Peso")]
-        public int Peso { get; set; }
-
+               
         [BsonElement("Endereco")]
         public string Endereco { get; set; } = null;
 
