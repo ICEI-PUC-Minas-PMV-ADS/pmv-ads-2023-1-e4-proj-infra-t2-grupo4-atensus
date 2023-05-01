89acf83d659da28f2d92486b73f7adf33da59075
Filipe Marques <62525275+filipe172020@users.noreply.github.com>
2023-05-01 16:46:21 -0300

    Add files via upload

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,20 @@
+import './Login.css';
+import LogoAtensus from '../../assets/logo atensus.png';
+
+const Login = () => {
+    return (
+        <div className="Principal1">
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family= Montserrat :wght@500;600;900 & display=swap');
+            </style>
+
+            <div className="Cabecalho">
+                <img src={LogoAtensus} className="Logo" />
+            </div>
+
+          
+        </div>
+    )
+}
+
+export default Login;
