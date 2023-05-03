3bafa3a2ae1f0b49a2f2d94872c4dc1cdc8408f9
André Soares Alves da silva <90812412+Andresoaresalves@users.noreply.github.com>
2023-05-03 01:40:42 -0300

    Mudanças Login.

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,44 @@
 import './Login.css';
 import LogoAtensus from '../../assets/logo atensus.png';
+import Medica from '../../assets/medica.png';
 
 const Login = () => {
     return (
-        <div className="Principal1">
-            <style>
-                @import url('https://fonts.googleapis.com/css2?family= Montserrat :wght@500;600;900 & display=swap');
-            </style>
+        <><div className="Principal">
 
             <div className="Cabecalho">
                 <img src={LogoAtensus} className="Logo" />
             </div>
 
-          
-        </div>
+            <img src={Medica} className="Imagem_Principal" />
+              
+            <div className='Div_Login'> 
+                   <h3> Entre ou Cadastre-se</h3>
+                    <form>
+                        <div className='Inputs1'>
+                    <div class="form">
+      <input type="text" class="form-control"  placeholder="Digite seu CPF"></input>
+      <label for="floatingInput"></label>
+    </div></div>
+    <div className='Inputs2'>
+    <div className="Form">
+      <input type="password" class="form-control"  placeholder="Digite a sua senha"></input>
+      <label for="FloatingPassword"> Digite a sua senha</label>
+    </div>
+    </div>
+  <div class="mb-3form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+    <label class="form-check-label" for="exampleCheck1">Lembrar senha </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Entrar</button>
+</form>
+<br/>
+<h4> Não tem cadastro? Registre-se</h4>
+</div>
+
+        </div><div className="Bolinha" /><div className="Bolinha1" /><div className="Bolinha2" /><div className="Bolinha3"/><div className="Bolinha4" /></>
+
+
     )
 }
 
