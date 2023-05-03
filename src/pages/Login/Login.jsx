9e098d142a3aaabbfb731bfe4a5edff9b5d18d3d
André Soares Alves da silva <90812412+Andresoaresalves@users.noreply.github.com>
2023-05-03 17:34:46 -0300

    Atualização do novo modelo de Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,42 +1,50 @@
 import './Login.css';
 import LogoAtensus from '../../assets/logo atensus.png';
-import Medica from '../../assets/medica.png';
+
 
 const Login = () => {
     return (
-        <><div className="Principal">
+<div className="Container">
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+</style>
 
-            <div className="Cabecalho">
-                <img src={LogoAtensus} className="Logo" />
-            </div>
-
-            <img src={Medica} className="Imagem_Principal" />
-              
-            <div className='Div_Login'> 
-                   <h3> Entre ou Cadastre-se</h3>
-                    <form>
-                        <div className='Inputs1'>
-                    <div class="form">
-      <input type="text" class="form-control"  placeholder="Digite seu CPF"></input>
-      <label for="floatingInput"></label>
-    </div></div>
-    <div className='Inputs2'>
-    <div className="Form">
-      <input type="password" class="form-control"  placeholder="Digite a sua senha"></input>
-      <label for="FloatingPassword"></label>
-    </div>
-    </div>
-  <div class="mb-3form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
-    <label class="form-check-label" for="exampleCheck1">Lembrar senha </label>
+<header className='Header'>
+  <img src={LogoAtensus} className='Logo'></img>
+</header>
+<h3> Entre ou Cadastre-se</h3>
+<forms>
+  <div className="InputContainer">
+    <label htmlFor="CPF">CPF</label>
+    <input 
+        type="text" 
+        Name="CPF" 
+        Id="CPF" 
+        placeholder=" Ex: 856.457.857-86">
+    </input>
   </div>
-  <button type="submit" class="btn btn-primary">Entrar</button>
-</form>
-<br/>
-<h4> Não tem cadastro? Registre-se</h4>
-</div>
+  <div className="InputContainer">
+    <label htmlFor="Password">Digite a sua Senha</label>
+    <input 
+        type="Password" 
+        Name="Password" 
+        Id="Password" 
+        placeholder="**********">
+    </input>
+  </div>
+  <button className="Button">Entrar</button>
+  <div className="footer"> 
+  <p> Não tem Cadastro? Registre-se aqui</p>
+  </div>
+  
+</forms>
+<div className="Bolinha" />
+<div className="Bolinha1" />
+<div className="Bolinha2" />
+<div className="Bolinha3" />
+<div className="Bolinha4" />
 
-        </div><div className="Bolinha" /><div className="Bolinha1" /><div className="Bolinha2" /><div className="Bolinha3"/><div className="Bolinha4" /></>
+</div>
 
 
     )
