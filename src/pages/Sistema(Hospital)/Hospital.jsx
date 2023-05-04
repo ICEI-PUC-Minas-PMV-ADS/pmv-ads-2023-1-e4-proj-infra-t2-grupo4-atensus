6687c4b047c6fa920881ee2d4e18dc5faa88f905
Filipe Marques <62525275+filipe172020@users.noreply.github.com>
2023-05-04 14:30:30 -0300

    Update Hospital.jsx

--- src/pages/Sistema(Hospital)/Hospital.jsx
+++ src/pages/Sistema(Hospital)/Hospital.jsx
@@ -1,75 +1,143 @@
-import './Hospital.css'
-import LogoAtensus from '../../assets/logo atensus.png';
 
-const Hospital = () => {
-    return (
-        <div className="Principal2">
-            <style>
-                @import url('https://fonts.googleapis.com/css2?family= Montserrat :wght@500;600;900 & display=swap');
-            </style>
-
-            <div className="Cabecalho2">
-                <img src={LogoAtensus} alt='Logo Atensus' className="Logo2" />
-            </div>
-
-            <div className="Gerenciamento">
-                <h1>Gerenciamento de pacientes</h1>
-            </div>
-
-            <div className="Paciente">
-                <h1 className="Texto_Paciente">Paciente:</h1>
-                <p className="Texto_Nome_Paciente">Luiza Almeida</p>
-                <form className="Especialidades" action="#">
-                    <select name="Especialidades" id="lang">
-                        <option value="Triagem">Triagem</option>
-                        <option value="Oftalmologista">Oftalmologista</option>
-                        <option value="Pediatria">Pediatria</option>
-                        <option value="Dermatologista">Dermatologista</option>
-                    </select>
-                </form>
-            </div>
-
-            <div className="Texto_Inputs">
-                <h1 className="Titulo_Texto_Inputs">Sintomas</h1>
-                <input type='text' className="Input"></input>
-                <h1>Observação</h1>
-                <input type='text' className="Input"></input>
-                <button className="btn_editar">Editar</button>
-            </div>
-
-
-            <h1 className="Texto_Agendamentos">Agendamentos</h1>
-            <div className="Agendamentos">
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 1</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 2</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 3</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 4</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 5</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 6</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 7</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 8</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 9</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 10</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 1</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 2</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 3</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 4</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 5</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 6</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 7</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 8</button>
-                <button className='btn_agendamentos'>Luiza Almeida ● Triagem ● Posição 9</button>
-
-            </div>
-
-            <div className="Bolinha" />
-            <div className="Bolinha1" />
-            <div className="Bolinha2" />
-            <div className="Bolinha3" />
-            <div className="Bolinha4" />
-
-        </div>
-    )
+.Principal2 {
+    display: flex;
+    width: 100%;
+    height: 100%;
 }
 
-export default Hospital;
+.Cabecalho2{
+    width: 100%;
+    height: 300px;
+}
+
+.Logo2 {
+    position: absolute;
+    width: 18%;
+    left: 52%;
+    top: -60px;
+    z-index: 1;
+    pointer-events: none;
+    background-color: transparent;
+}
+
+.Gerenciamento {
+    position: absolute;
+    text-align: center;
+    margin-top: 9%;
+    margin-left: 45%;
+}
+
+.Paciente{
+    width: 100%;
+    position: absolute;
+    margin-top: 12%;
+    margin-left: 50%;
+}
+
+.Texto_Paciente {
+    display: inline;
+    background-color: transparent;
+    color: #118FB8;
+    margin-right: 1%;
+    font-size: 25px;
+}
+
+.Texto_Nome_Paciente {
+    display: inline;
+    background-color: transparent;
+    color: #3c3c3c;
+    font-size: 25px;
+}
+
+.Texto_Inputs {
+    position: absolute;
+    width: 800px;
+    height: 100%;
+    background-color: transparent;
+    margin-top: 19%;
+    margin-left: 45%;
+}
+
+.Titulo_Texto_Inputs {
+    margin-right: 30%;
+    font-size: 25px;
+    color: #3c3c3c;
+    background-color: transparent;
+}
+
+.Especialidades {
+   margin-left: 5%;
+   margin-top: 1%;
+}
+
+.Especialidades select {
+    border: none; 
+    width: 200px;
+    height: 40px;
+    background-color:#73c2ab;
+    color: white;
+    border-radius: 10px;
+    outline: 0;
+    text-align: center;
+}
+
+.Especialidades option {
+    background-color:#73c2ab;
+    border: none; 
+}
+
+.Input {
+    margin-right: 30%;
+    margin-bottom: 3%;
+    margin-top: 2%;
+    width: 500px;
+    height: 100px;
+    border: none;
+    background-color: #F1F1F1;
+    border-radius: 10px;
+    outline: 0;
+    text-align: center;
+}
+
+.Texto_Agendamentos {
+    position: fixed;
+    margin-left: 11%;
+    margin-top: 3%;
+    color: #118FB8;
+    background-color: transparent;
+}
+
+.Agendamentos {
+    position: absolute;
+    height: 610px;
+    top: 13%;
+    left: 10%;
+    z-index: 1;
+    overflow-y: scroll;
+    background-color: transparent;
+}
+
+.btn_agendamentos {
+    display: block;
+    width: 300px;
+    height: 50px;
+    margin-top: 10%;
+    border: none;
+    background-color: #3c3c3c;
+    border-radius: 10px;
+    color: white;
+    
+}
+
+.btn_editar {
+    display: block;
+    width: 100px;
+    height: 25px;
+    margin-left: 26%;
+    margin-top: 3%;
+    border: none;
+    background-color: #3c3c3c;
+    color: white;
+    border-radius: 5px;
+}
+
