e3b5134d92bb387f8f05b42d6346027cdaf98b46
TiagoMiqueiasDesenvolvedor <tiagomiqueiasrodrigues@gmail.com>
2023-05-13 19:54:10 -0300

    Ação-botão-login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,25 @@
 import './Login.css';
 import LogoAtensus from '../../assets/logo atensus.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Medico from '../../assets/medico.png';
 import { useState } from 'react';
 import api from '../../lib/axios';
 
 const Login = () => {
+  const navigate = useNavigate(); // Importe o useNavigate
   const [cpf, setCpf] = useState('');
   const [senha, setSenha] = useState('');
+  const [erro, setErro] = useState(false); // Crie um estado para armazenar se houve um erro de login
 
   async function handleLogin(event) {
     event.preventDefault();
     try {
       const response = await api.get(`/pacientes/login?CPF=${cpf}&senha=${senha}`);
       console.log(response.data);
-      // Redirecionar para página de Hospital
+      navigate('/Agendamento'); // Navegue para a página de agendamentos em caso de sucesso
     } catch (error) {
       console.log(error);
+      setErro(true); // Configure o estado de erro como verdadeiro em caso de falha
     }
   }
 
@@ -47,7 +50,6 @@ const Login = () => {
               placeholder=" Ex: 856.457.857-86">
             </input>
           </div>
-         
 
           <div className="InputContainer">
             <label htmlFor="Password">Digite a sua Senha</label>
@@ -61,10 +63,11 @@ const Login = () => {
             </input>
           </div>
 
+          {erro && <p>Faça seu cadastro!</p>} {/* Exibe a mensagem de erro se o estado de erro for verdadeiro */}
           
           <button type="submit" className="Button">Entrar</button>
           <div className="footer">
-           <Link to="/Cadastro"> <p> Não tem Cadastro? Registre-se aqui</p></Link>
+            <Link to="/Cadastro"><p> Não tem Cadastro? Registre-se aqui</p></Link>
           </div>
         </form>
       </div>
