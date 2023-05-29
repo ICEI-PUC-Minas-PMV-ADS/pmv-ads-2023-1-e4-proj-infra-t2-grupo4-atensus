cf249638ee6ad307ffa9b3f8e27f8edc6801e93f
Filipe marques <filipemarquespascoal@hotmail.com>
2023-05-29 12:36:48 -0300

    rotas e estrutura

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './Pages/Login/indexLogin';
+import IndexAgendamentoCliente from './Pages/Agendamentos(Cliente)';
 
 export default function App() {
 
@@ -10,7 +11,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Login" component={Login} /> */}
+        <Stack.Screen name="AgendamentoCliente" component={IndexAgendamentoCliente} />
       </Stack.Navigator>
     </NavigationContainer>
 
---
+++ Pages/Agendamentos(Cliente)/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function IndexAgendamentoCliente() {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+                <StatusBar barStyle='light-content' />
+                <Text>oi</Text>
+        </SafeAreaView>
+    )
+}
