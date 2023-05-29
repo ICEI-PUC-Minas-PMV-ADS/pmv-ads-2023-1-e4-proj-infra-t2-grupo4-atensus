d957317c8ed6a610a2b5acec929813e75b7d2bf8
Filipe Marques <62525275+filipe172020@users.noreply.github.com>
2023-05-29 12:06:16 -0300

    Add files via upload

---
+++ Pages/Login/indexLogin.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Login() {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+                <StatusBar barStyle='light-content' />
+            <View style={styles.container}>
+                <Text>Oi testando</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
