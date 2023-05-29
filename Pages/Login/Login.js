9158fa73788062a0b3a9098ec3c0848582efb1a3
André Soares Alves da silva <90812412+Andresoaresalves@users.noreply.github.com>
2023-05-29 15:35:51 -0300

    Add files via upload

---
+++ Pages/Login/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { 
+  KeyboardView, 
+  Title, 
+  Container, 
+  Input, 
+  ButtonSubmit,
+  TextButton,
+  Text
+} from './Styles';
+
+function Login() {
+  return (
+     <KeyboardView>
+      <Container>
+        <Title>Login</Title>
+        <Input 
+          placeholderTextColor="#000"
+          placeholder="CPF"
+        />
+        <Input 
+          placeholderTextColor="#000"
+          placeholder="Senha"
+          secureTextEntry
+        />
+        <ButtonSubmit>
+          <TextButton>
+            Entrar
+          </TextButton>
+        </ButtonSubmit>
+        <Title>Não tem cadastro? Registre-se Aqui!</Title>
+      </Container>
+    </KeyboardView>
+    )
+}
+
+export default Login;
