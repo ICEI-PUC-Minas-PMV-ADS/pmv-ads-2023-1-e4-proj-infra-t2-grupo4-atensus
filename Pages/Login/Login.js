8659a304cd46e47a264faf7aefe0962e4b316a14
TiagoMiquéias <tiago.rodrigues.1338413@sga.pucminas.br>
2023-06-04 15:04:31 -0300

    mudança

--- Pages/Login/Login.js
+++ Pages/Login/Login.js
@@ -1,131 +1,145 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from "expo-status-bar";
-
-
-
+import api from '../../services/api';
 
 export default function Login() {
+  const navigation = useNavigation();
+  const [cpf, setCpf] = useState('');
+  const [senha, setSenha] = useState('');
+  const [erro, setErro] = useState(false); // Crie um estado para armazenar se houve um erro de login
 
-    const navigation = useNavigation();
+  async function handleLogin() {
+    try {
+      
 
-    return (
 
-        <View style={styles.containerLogin}>
-            <StatusBar barStyle='light-content' />
+      const url = `/pacientes/login?CPF=${cpf}&senha=${senha}`;
+      console.log(url);
+      const response = await api.get(url);
 
-            <Image style={styles.imagemLogoLogin} source={require('../../assets/splash.png')}></Image>
-            <Text style={styles.textoLogin}>Login</Text>
+      // const response = await api.get(`/pacientes/login?CPF=${cpf}&senha=${senha}`);
+      console.log("Conectado");
+      navigation.navigate('Agendamento'); // Navegue para a página de agendamentos em caso de sucesso
+    } catch (error) {
+      console.log(error);
+      setErro(true); // Configure o estado de erro como verdadeiro em caso de falha
+    }
+  }
 
-            <View style={styles.containerInputs}>
+  return (
+    <View style={styles.containerLogin}>
+      <StatusBar barStyle='light-content' />
 
-                <Text style={styles.textoInputUsuario}>Usuário</Text>
+      <Image style={styles.imagemLogoLogin} source={require('../../assets/splash.png')}></Image>
+      <Text style={styles.textoLogin}>Login</Text>
 
-                <TextInput
-                    style={styles.inputUsuario}    
-                />
+      <View style={styles.containerInputs}>
+        <Text style={styles.textoInputUsuario}>Usuário</Text>
+        <TextInput
+          style={styles.inputUsuario}
+          value={cpf}
+          onChangeText={setCpf}
+        />
 
-                <Text style={styles.textoInputSenha}>Senha</Text>
+        <Text style={styles.textoInputSenha}>Senha</Text>
+        <TextInput
+          style={styles.inputSenha}
+          secureTextEntry={true}
+          value={senha}
+          onChangeText={setSenha}
+        />
 
-                <TextInput
-                    style={styles.inputSenha}  
-                    secureTextEntry={true}               
-                />
+        <TouchableOpacity style={styles.buttonEntrarLogin} onPress={handleLogin}>
+          <Text style={styles.textoEntrarLogin}>Entrar</Text>
+        </TouchableOpacity>
 
-                <TouchableOpacity style={styles.buttonEntrarLogin} onPress={() => navigation.navigate('TelaInicial', { screen: 'TelaInicial' })}>
-                    <Text style={styles.textoEntrarLogin}>Entrar</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity style={styles.buttonRegistrarLogin}onPress={() => navigation.navigate('Cadastro', { screen: 'Cadastro' })}>
-                    <Text style={styles.textoRegistrarLogin}>Registre-se</Text>
-                </TouchableOpacity>
-
-            </View>
-
-        </View>
-    )
+        <TouchableOpacity style={styles.buttonRegistrarLogin} onPress={() => navigation.navigate('Cadastro', { screen: 'Cadastro' })}>
+          <Text style={styles.textoRegistrarLogin}>Registre-se</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    containerLogin: {
-        flex: 1,
-        backgroundColor: '#2B2B2B',
-        alignItems: 'center',
-    },
-    textoLogin: {
-        marginTop: '10%',
-        fontSize: 18,
-        fontWeight: 700,
-        color: 'white'
-    },
-    textoEntrarLogin: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 600
-    },
-    textoRegistrarLogin: {
-        color: '#5C5C5C',
-        fontSize: 15,
-    },
-    textoInputUsuario: {
-        bottom: '9%',
-        right: '33%',
-        fontSize: 15
-    },
-    textoInputSenha: {
-        top: '1%',
-        right: '33%',
-        fontSize: 15
-    },
-    imagemLogoLogin: {
-        width: 350,
-        height: 150,
-        top: '7%',
-        left: '4%'
-    },
-    containerInputs: {
-        width: 350,
-        height: 550,
-        top: '3%',
-        backgroundColor: 'white',
-        borderRadius: 25,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonEntrarLogin: {
-        backgroundColor: '#118FB8',
-        width: 200,
-        height: 60,
-        top: '13%',
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonRegistrarLogin: {
-        width: 100,
-        height: 30,
-        top: '18%',
-        borderRadius: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    inputUsuario: {
-        width: 300,
-        height: 70,
-        backgroundColor: '#EDEDED',
-        borderRadius: 10,
-        bottom: '7%',
-        textAlign: 'center'
-    },
-    inputSenha: {
-        width: 300,
-        height: 70,
-        backgroundColor: '#EDEDED',
-        borderRadius: 10,
-        textAlign: 'center',
-        top: '3%',
-
-    }
-
-})
+  containerLogin: {
+    flex: 1,
+    backgroundColor: '#2B2B2B',
+    alignItems: 'center',
+  },
+  textoLogin: {
+    marginTop: '10%',
+    fontSize: 18,
+    fontWeight: '700',
+    color: 'white'
+  },
+  textoEntrarLogin: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '600'
+  },
+  textoRegistrarLogin: {
+    color: '#5C5C5C',
+    fontSize: 15,
+  },
+  textoInputUsuario: {
+    bottom: '9%',
+    right: '33%',
+    fontSize: 15
+  },
+  textoInputSenha: {
+    top: '1%',
+    right: '33%',
+    fontSize: 15
+  },
+  imagemLogoLogin: {
+    width: 350,
+    height: 150,
+    top: '7%',
+    left: '4%'
+  },
+  containerInputs: {
+    width: 350,
+    height: 550,
+    top: '3%',
+    backgroundColor: 'white',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonEntrarLogin: {
+    backgroundColor: '#118FB8',
+    width: 200,
+    height: 60,
+    top: '13%',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonRegistrarLogin: {
+    width: 100,
+    height: 30,
+    top: '18%',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputUsuario: {
+    width: 300,
+    height: 70,
+    backgroundColor: '#EDEDED',
+    borderRadius: 10,
+    bottom: '7%',
+    textAlign: 'center'
+  },
+  inputSenha: {
+    width: 300,
+    height: 70,
+    backgroundColor: '#EDEDED',
+    borderRadius: 10,
+    textAlign: 'center',
+    top: '3%',
+  }
+});
