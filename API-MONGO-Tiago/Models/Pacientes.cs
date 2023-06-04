e28c2486f03482b0f7f577e39914433ef4361e2a
TiagoMiquéias <tiago.rodrigues.1338413@sga.pucminas.br>
2023-06-04 20:42:17 -0300

    teste

---
+++ API-MONGO-Tiago/Models/Pacientes.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace API_AtenSUS.Models
+{
+    public class Pacientes
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Nome")]
+        public string Nome { get; set; }
+
+        [BsonElement("Idade")]
+        public int Idade { get; set; }
+
+        [BsonElement("Altura")]
+        public double Altura { get; set; }
+
+        [BsonElement("Peso")]
+        public double Peso { get; set; }
+
+        [BsonElement("Endereco")]
+        public string Endereco { get; set; } = null;
+
+        [BsonElement("Sintomas")]
+        public string Sintomas { get; set; } = null;
+    }
+}
---
+++ API-MONGO-Tiago/Program.cs
@@ -0,0 +1,33 @@
+using API_AtenSUS.Models;
+using API_AtenSUS.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.Configure<PacientesDatabaseSettings>
+    (builder.Configuration.GetSection("DevNetStoreDatabase")); //Objeto pego do appsettings.json
+
+builder.Services.AddSingleton<PacientesServices>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
